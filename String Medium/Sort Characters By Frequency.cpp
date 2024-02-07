84c24f12d9869d31c55f073f9a0e7e09ebd68f32
Krish Tak <takrishtak2002@gmail.com>
2024-02-08 00:10:21 +0530

    Solved String Medium question

---
+++ String Medium/Sort Characters By Frequency.cpp
@@ -0,0 +1,53 @@
+// Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+// Return the sorted string. If there are multiple answers, return any of them.
+
+ 
+
+// Example 1:
+
+// Input: s = "tree"
+// Output: "eert"
+// Explanation: 'e' appears twice while 'r' and 't' both appear once.
+// So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+// Example 2:
+
+// Input: s = "cccaaa"
+// Output: "aaaccc"
+// Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+// Example 3:
+
+// Input: s = "Aabb"
+// Output: "bbAa"
+// Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 5 * 105
+// s consists of uppercase and lowercase English letters and digits.
+
+// Leetcode
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        priority_queue<pair<int,char>> pq;
+        unordered_map<char,int> mp;
+        for(auto &x : s) mp[x]++;
+        for(auto &x : mp) pq.push({x.second,x.first});
+        string ans;
+        while(!pq.empty()) {
+            pair<int,char> p = pq.top();
+            int freq = p.first;
+            char ch = p.second;
+            pq.pop();
+            for(int i=0;i<freq;i++){
+                ans += ch;
+            }
+        }
+        return ans;
+    }
+};
